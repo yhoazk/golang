0bf47117026e97a74adf721436e70ba8ee6f8d0a
Gustavo Espindola <yhoazk@gmail.com>
2017-01-04 12:02:13 -0600

    Go values from https://gobyexample.com

---
+++ values/values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("go lang" + "<- concatenate with plus sign")
+	fmt.Println("1+1", 1+1)
+	fmt.Println("Floats 3.45 + 7.888", 3.45 + 7.888)
+
+	fmt.Println("true || false", true || false)
+	fmt.Println("!false", !false)
+}
